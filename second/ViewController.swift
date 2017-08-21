c7c67b1882f44fa7c3b2102835da8808519c49c7
Release <release@releaseui-imac-60.local>
2017-08-21 10:35:47 +0900

    Initial Commit

---
+++ second/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  second
+//
+//  Created by Release on 2017. 8. 21..
+//  Copyright © 2017년 hjy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
